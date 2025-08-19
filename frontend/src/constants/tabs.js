07afef66dbdfe47cb4723c3616b556f491595ab1
sielskibartosz <sielskibartosz95@gmail.com>
2025-08-19 10:06:06 +0200

    development branch

--- frontend/src/constants/tabs.js
+++ frontend/src/constants/tabs.js
@@ -1,4 +1,5 @@
 // src/constants/tabs.js
+import customFoto from '../assets/custom_foto.jpg';
 export const TABS = [
     { key: "id", labelKey: "id_tab", aspect: "35/45" },
     { key: "custom", labelKey: "custom_tab", aspect: "50/50" },
@@ -9,10 +10,10 @@ export const TAB_DESCRIPTION = {
     title: "35/45 mm",
     description: "",
     link: "https://www.gov.pl/web/gov/zdjecie-do-dowodu-lub-paszportu",
-    image: "/images/IDsolo.png",
+    image: customFoto,
   },
   custom: {
     title: "",
-    image: "/images/custom_foto.jpg",
+    image: customFoto,
   },
 };
---
+++ frontend/vite.config.js
@@ -0,0 +1,2 @@
+import {defineConfig, loadEnv} from 'vite'
+import react from '@vitejs/plugin-react'
