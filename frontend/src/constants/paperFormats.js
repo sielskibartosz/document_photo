fd89f0bf71899f2913fbe8414c7044d80fa0d26d
sielskibartosz <sielskibartosz95@gmail.com>
2026-03-26 20:45:11 +0100

    paper formats change 10.2 ->10

--- frontend/src/constants/paperFormats.js
+++ frontend/src/constants/paperFormats.js
@@ -1,5 +1,5 @@
 export const PAPER_FORMATS = {
-    "10x15 cm Rossmann": {width: 10.2, height: 15.2},
+    "10x15 cm Rossmann": {width: 10, height: 15},
     "13x18 cm": {width: 13, height: 18},
     "15x21 cm": {width: 15, height: 21},
     "20x30 cm": {width: 20, height: 30},
